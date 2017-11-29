e507ed6a434facfe5c78c760d8dabdc72b6615ba
Ronaldo Richieri <richieri@gmail.com>
2017-11-29 18:20:34 -0200

    adjust on Readme.md

--- README.md
+++ README.md
@@ -3,9 +3,7 @@
 This repository brings to you a OTRS 6 Dockerfile, without database instalation.
 
 If you want a really easy OTRS Docker installation, please check this other option, with Database deploy:
-docker.otrs.withdatabase (available soon)
-
-This GitHub repository is connected to Docker HUB, so you need nothing from here actually.
+docker.easy.otrs (available soon)
 
 If you just want to run a docker container with our OTRS 6 flavor, follow these steps:
 
@@ -15,6 +13,7 @@ If you just want to run a docker container with our OTRS 6 flavor, follow these 
 
  2. Run the following command:
 
-`docker run -ti ligero/otrs:6.0.1`
+`docker run -tid -v otrs_data:/opt/otrs -p 80:80 ligero/otrs`
 
 If you are running your docker container on your localhost, go to http://localhost/otrs/installer.pl to proceed with the rest of the installation.
+
--- otrs.sh
+++ otrs.sh
@@ -15,16 +15,16 @@ if [ ! -f "/installed" ]  ; then
     
     /opt/otrs/bin/otrs.SetPermissions.pl --web-group=www-data
 
-    touch "/installed"
-fi
-    
-# Create default files if first time
-for file in "Kernel/Config.pm" "var/cron/aaa_base"  "var/cron/otrs_daemon"; do
-    if [ ! -e "/opt/otrs/${file}" ] ; then
-         cp  "/opt/src/otrs/${file}.dist" "/opt/otrs/${file}"
-         chown otrs:www-data "/opt/otrs/${file}"
-         chmod 0666 "/opt/otrs/${file}"
-    fi
-done
+    # Create default files if first time
+    for file in "Kernel/Config.pm" "var/cron/aaa_base"  "var/cron/otrs_daemon"; do
+        if [ ! -e "/opt/otrs/${file}" ] ; then
+             cp  "/opt/src/otrs/${file}.dist" "/opt/otrs/${file}"
+             chown otrs:www-data "/opt/otrs/${file}"
+             chmod 0666 "/opt/otrs/${file}"
+        fi
+    done
 
+    touch "/installed"
+    
+fi
 /usr/bin/supervisord
