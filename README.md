ce1c460f907f002ee6de80588ef41013e13fbaae
Ronaldo Richieri <richieri@gmail.com>
2018-07-27 14:34:51 -0300

    OTRS v6.0.9 with LigeroRepository and instalation improvements

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-﻿[![N|Solid](https://i1.wp.com/complemento.net.br/wp-content/uploads/2017/11/logo_otrs6free.png?fit=300%2C68&ssl=1)]()
+﻿
+[![N|Solid](https://i1.wp.com/complemento.net.br/wp-content/uploads/2017/11/logo_otrs6free.png?fit=300%2C68&ssl=1)]()
 
 OTRS Docker Installation
 ========================
 
-What's OTRS?
-------------
+## What's OTRS?
 
 OTRS is the world most popular Service Desk software, delivered by OTRS Group and a large open source community. You can check more information and all OTRS Group Services in their web site:
 http://www.otrs.com
@@ -12,46 +12,49 @@ http://www.otrs.com
 This Docker image is maintained by Complemento, a Brazilian company dedicate to delivery ITSM with open source software. We develop many Free and Enterprise AddOns for OTRS. You can check our website for more information:
 http://www.complemento.net.br
 
-About this Image
-----------------
-This image aims to make a fresh new application install of OTRS. You will need to create another mysql container for your own (or use an traditional mysql server/service).
+## About this Image
+
+This image aims to make a fresh new application install of OTRS. You will need to create another mysql container for your own or use our docker-compose file.
 
 If you want a really easy OTRS Docker installation for testing purposes, please check this other option, with Database deployment included in the same container:
 
 https://hub.docker.com/r/ligero/otrs_easy/
 
-If you just want to run a docker container with our OTRS 6 flavor, follow these steps:
+## Running this Image
 
- 1. Install a docker server. You can download Docker Community Edition from this link: 
-
+You can run this image in 2 different ways: with docker-compose, creating a stack with OTRS and Mysql, or without docker-compose, running a standalone container and finishing OTRS installation through it's web interface.
+First of all, please Install a docker server. You can download Docker Community Edition from this link: 
 	https://www.docker.com/community-edition#/download
+
+Now, choose the way you want to run this image:
+
+### Using docker-compose
+Please make sure you have installed docker-compose:
+https://docs.docker.com/compose/install/#install-compose
+
+Download the docker-compose.yaml file from our github, to some folder of your preference. Let's suppose you want to put it under /opt/containers/otrs:
+
+`mkdir -p /opt/containers/otrs`
+
+`cd /opt/containers/otrs`
+
+`wget https://raw.githubusercontent.com/complemento/docker.otrs/master/docker-compose.yaml`
+
+`docker-compose up`
+
+
+### Standalone container
+
+If you just want to run a docker container with our OTRS 6 flavor, follow these steps:
 
  2. Run the following command:
 
 `docker run -d -v otrs_data:/opt/otrs -p 80:80 ligero/otrs`
 
- - In production environments, we recommend you to set the version of your choice:
- 
- `docker run -d -v otrs_data:/opt/otrs -p 80:80 ligero/otrs:`
- 
- Currently available versions:
- - 
- - 5.0.26
-
-Older not maintained versions:
- - 
- - 
- - 
- - 
- - 6.0.5
- - 6.0.4
- - 6.0.3
- - 6.0.2
- - 6.0.1
- 
 If you are running your docker container on your localhost, go to http://localhost/otrs/installer.pl to proceed with the rest of the installation.
 
 
+## Other settings
 A more complex way of running this image:
 ```
 docker run -d --name sd_otrs_app \
--- otrs.sh
+++ otrs.sh
@@ -5,20 +5,27 @@ line=$(cat /etc/hosts | grep `hostname`)
 line2=$(echo $line | awk '{print $2}')
 echo "$line $line2.localdomain" >> /etc/hosts
 
-if [ ! -f "/installed" ]  ; then
-
-    # OTRS installation
-    su -c "/opt/src/link.pl /opt/src/otrs /opt/otrs" -s /bin/bash otrs
+# Verifies if we already executed installation process
+if [ ! -f "/opt/otrs/installed" ]  ; then
+    # COPY OTRS FILES TO DESTINATION FOLDER
+    /opt/src/link.pl /opt/src/otrs /opt/otrs
     rm /opt/otrs/bin/otrs.SetPermissions.pl
     cp /opt/src/otrs/bin/otrs.SetPermissions.pl /opt/otrs/bin/otrs.SetPermissions.pl
 
+    # Fix permissions
+    cd /opt/otrs
+    # if articles are mapped as a volume, it's not a good idea to change permissions
+    # now because it can take a long time
+    find . ! -path "*var/article/*" | xargs chown otrs:www-data
+    
     ## Create missing directories from GIT
     for dir in "/opt/otrs/var/spool" "/opt/otrs/var/tmp" "/opt/otrs/var/article"; do
         mkdir "${dir}"
         chown otrs:www-data "${dir}"
     done
     
-    /opt/otrs/bin/otrs.SetPermissions.pl --web-group=www-data
+    # For some reason, we need to set permissions again :/
+    /opt/otrs/bin/otrs.SetPermissions.pl --web-group=www-data --skip-article-dir
 
     # Create default files if first time
     for file in "Kernel/Config.pm" "var/cron/aaa_base"  "var/cron/otrs_daemon"; do
@@ -29,7 +36,71 @@ if [ ! -f "/installed" ]  ; then
         fi
     done
 
-    touch "/installed"
+    ## Initital Install
+    # Check if INSTALL variable is set (from our docker-compose file for example)
+    if [ -n "${INSTALL}" ]; then
+
+        ##### Put the variables on OTRS Config.pm
+        sed -i 's/\(.*{DatabaseHost}.*\)127.0.0.1/\1'"${MYSQL_HOSTNAME}"'/' /opt/otrs/Kernel/Config.pm
+        sed -i 's/\(.*{Database}.*\)otrs/\1'"otrs_${CUSTOMER_ID}"'/' /opt/otrs/Kernel/Config.pm
+        sed -i 's/\(.*{DatabaseUser}.*\)otrs/\1'"${MYSQL_USERNAME}"'/' /opt/otrs/Kernel/Config.pm
+        sed -i 's/\(.*{DatabasePw}.*\)some-pass/\1'"${MYSQL_PASSWORD}"'/' /opt/otrs/Kernel/Config.pm
+            
+        # Waits mysql to be running for proceding Database instalation
+        while ! mysqladmin ping -h "${MYSQL_HOSTNAME}" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" -u"${MYSQL_USERNAME}" --silent; do sleep 1; echo "Wainting Mysql...\n"; done
+
+        
+        ##### Install OTRS Database ####
+        mysql -h "${MYSQL_HOSTNAME}" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" -e "CREATE DATABASE otrs_${CUSTOMER_ID} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" && \
+        mysql -h "${MYSQL_HOSTNAME}" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" otrs_${CUSTOMER_ID} < /opt/src/otrs/scripts/database/otrs-schema.mysql.sql && \
+        mysql -h "${MYSQL_HOSTNAME}" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" otrs_${CUSTOMER_ID} < /opt/src/otrs/scripts/database/otrs-initial_insert.mysql.sql && \
+        mysql -h "${MYSQL_HOSTNAME}" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" otrs_${CUSTOMER_ID} < /opt/src/otrs/scripts/database/otrs-schema-post.mysql.sql
+
+
+        ###### SysConfig defaults parameters ##############
+        su -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs;    
+        if [ -n "${OTRS_DEFAULT_LANGUAGE}" ]; then
+                su -c "/opt/otrs/bin/otrs.Console.pl Admin::Config::Update --no-deploy --setting-name DefaultLanguage --value ${OTRS_DEFAULT_LANGUAGE}" -s /bin/bash otrs;
+        fi
+        
+        if [ -n "${OTRS_FQDN}" ]; then
+            su -c "/opt/otrs/bin/otrs.Console.pl Admin::Config::Update --no-deploy --setting-name FQDN --value ${OTRS_FQDN}" -s /bin/bash otrs;
+        fi
+        
+        if [ -n "${OTRS_SYSTEM_ID}" ]; then
+            su -c "/opt/otrs/bin/otrs.Console.pl Admin::Config::Update --no-deploy --setting-name SystemID --value ${OTRS_SYSTEM_ID}" -s /bin/bash otrs;
+        fi
+
+        ### Allow community packages to be installed
+   		su -c "sed -i '26i\ \ \ \ delete \$Self->{\x27Frontend::NotifyModule\x27}->{\x278000-PackageManager-CheckNotVerifiedPackages\x27};' /opt/otrs/Kernel/Config.pm" -s /bin/bash otrs;
+        # just add a new blank line
+        su -c "sed -i '26i\ \ \ \ ' /opt/otrs/Kernel/Config.pm" -s /bin/bash otrs;
+
+        su -c "/opt/otrs/bin/otrs.Console.pl Admin::Config::Update --no-deploy --setting-name 'Package::AllowNotVerifiedPackages' --value 1" -s /bin/bash otrs;
+
+        # For some strange reason, we need to Rebuild config twice in some systems
+        su -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs;    
+        su -c "/opt/otrs/bin/otrs.Console.pl Admin::Config::Update --no-deploy --setting-name SecureMode --value 1" -s /bin/bash otrs;
+        su -c "/opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs;
+        
+        ### OTRS admin default password:
+        su -c "/opt/otrs/bin/otrs.Console.pl Admin::User::SetPassword 'root@localhost' ligero" -s /bin/bash otrs;
+
+        ### Install LigeroRepository
+        if [ ! -n "${DONT_INSTALL_LIGERO_ADDONS}" ]; then
+            for addon in /opt/ligero_addons/*.opm; do
+                su -c "/opt/otrs/bin/otrs.Console.pl Admin::Package::Install $addon" -s /bin/bash otrs;
+            done
+        fi
+
+        ### Install Packages
+        for addon in /opt/otrs_addons/*.opm; do
+            su -c "/opt/otrs/bin/otrs.Console.pl Admin::Package::Install $addon" -s /bin/bash otrs;
+        done
+
+    fi
+
+    touch "/opt/otrs/installed"
     
 fi
 /usr/bin/supervisord
