7d65776c260b9f3d420f70aebe803892c1375af9
Marcelo Matos <marcelo.matos@complemento.net.br>
2019-04-09 15:06:27 -0300

    hide connection test errors

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 printenv | grep APP_ | sed 's/^\(.*\)$/export \1/g' > /etc/profile.d/app-env.sh
 
 # database connection test
-while ! su -c "otrs.Console.pl Maint::Database::Check" otrs; 
+while ! su -c "otrs.Console.pl Maint::Database::Check" otrs 2> /dev/null; 
 do 
     sleep 1;
 done
