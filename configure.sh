062fe80da7b4a672cc7802b81438df90ebb77717
Marcelo Matos <marcelo.matos@complemento.net.br>
2019-04-09 15:01:55 -0300

    load default data

---
+++ configure.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# Database installation
+case $APP_DatabaseType in
+
+mysql)
+    
+    echo " * Loading MySQL data"
+    
+    mysql -h "${APP_DatabaseHost}" -u "${APP_DatabaseUser}" -p"${APP_DatabasePw}" ${APP_Database} < /opt/otrs/scripts/database/otrs-schema.mysql.sql \
+    && mysql -h "${APP_DatabaseHost}" -u "${APP_DatabaseUser}" -p"${APP_DatabasePw}" ${APP_Database} < /opt/otrs/scripts/database/otrs-initial_insert.mysql.sql \
+    && mysql -h "${APP_DatabaseHost}" -u "${APP_DatabaseUser}" -p"${APP_DatabasePw}" ${APP_Database} < /opt/otrs/scripts/database/otrs-schema-post.mysql.sql
+    
+    [ $? -gt 0 ] && echo "Error loading MySQL data" && exit 1;
+    
+    ;;
+
+postgresql)
+
+    #TODO
+    echo " * Loading PostgreSQL data"
+    #psql -h "${APP_DatabaseHost}" -u "${APP_DatabaseUser}" -p"${APP_DatabasePw}" "CREATE DATABASE ${APP_Database}"
+    ;;
+
+oracle)
+
+    #TODO
+    echo " * Loading ORACLE data"
+    ;;
+
+*) 
+    echo " * APP_DatabaseType is not set";
+    exit 1;
+esac;
+
+# install otrs packages
+for PKG in `ls -1 /opt/otrs/var/packages/*.opm`; do
+    echo " * Installing package $PKG"
+    su -c "otrs.Console.pl Admin::Package::Install $PKG" otrs;
+done;
+
+# otrs root password
+su -c "otrs.Console.pl Admin::User::SetPassword 'root@localhost' complemento" otrs;
+echo "Password: complemento"
