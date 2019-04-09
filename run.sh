b2ecd673cb047929ef912312499ff6a782647f5d
Marcelo Matos <marcelo.matos@complemento.net.br>
2019-04-09 12:09:59 -0300

    Testing database connection before container starts

--- run.sh
+++ run.sh
@@ -3,5 +3,11 @@
 # set APP ENV vars
 printenv | grep APP_ | sed 's/^\(.*\)$/export \1/g' > /etc/profile.d/app-env.sh
 
+# database connection test
+while ! su -c "otrs.Console.pl Maint::Database::Check" otrs; 
+do 
+    sleep 1;
+done
+
 # run services
 exec supervisord
