26e36ad846902624faa5cb9de2fe635e78ae4dc7
Marcelo Matos <marcelo.matos@complemento.net.br>
2019-04-22 16:45:16 -0300

    allow not verified packages

--- app-init.sh
+++ app-init.sh
@@ -29,6 +29,8 @@ postgresql)
 esac;
 
 # install packages
+otrs.Console.pl Admin::Config::Update --setting-name 'Package::AllowNotVerifiedPackages' --value 1 --no-deploy
+otrs.Console.pl Maint::Config::Rebuild
 for PKG in `ls -1 /opt/otrs/var/packages/*.opm`; do
     echo "$0 - Installing package $PKG"
     otrs.Console.pl Admin::Package::Install --force --quiet $PKG
