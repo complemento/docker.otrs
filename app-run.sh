27396334e239c041226e5f11f7da3704bee984a7
mfmatos <46325500+mfmatos@users.noreply.github.com>
2020-01-21 13:52:29 -0300

    run SetPermissions before database test

--- app-run.sh
+++ app-run.sh
@@ -14,6 +14,9 @@ INITSCREEN_PID=$!
 # set APP ENV vars
 printenv | grep APP_ | sed 's/^\(.*\)$/export \1/g' > /etc/profile.d/app-env.sh
 
+# fix permissions before database test
+perl /opt/otrs/bin/otrs.SetPermissions.pl --skip-article-dir
+
 # database connection test
 while ! su -c "otrs.Console.pl Maint::Database::Check" otrs 2> /tmp/console-maint-database-check.log; 
 do
@@ -22,8 +25,6 @@ do
     # init configuration if empty
     grep "database content is missing" /tmp/console-maint-database-check.log \
     && su -c "/app-init.sh" otrs;
-
-    perl /opt/otrs/bin/otrs.SetPermissions.pl
     
     sleep 1;
 done
