2acc5776c936e586ce28e8203ddd8d252d3a9d74
Marcelo Matos <marcelofmatos@gmail.com>
2020-01-28 22:21:55 -0300

    cache loader removed

--- app-init.sh
+++ app-init.sh
@@ -78,6 +78,4 @@ otrs.Console.pl Admin::User::SetPassword 'root@localhost' ${ROOT_PASSWORD:-liger
 echo "Password: ligero"
 unset ROOT_PASSWORD
 
-otrs.Console.pl Maint::Loader::CacheGenerate
-
 echo "98" > $PROGRESSBAR_FILE
