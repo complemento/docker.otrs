2e9609c4ed942f4403ae4c963788d4ad11cf2db3
mfmatos <46325500+mfmatos@users.noreply.github.com>
2021-01-31 14:21:06 -0300

    Update app-run.sh

--- app-run.sh
+++ app-run.sh
@@ -18,18 +18,19 @@ INITSCREEN_PID=$!
 printenv | grep APP_ | sed 's/^\(.*\)$/export \1/g' > /etc/profile.d/app-env.sh
 
 # database connection test
-while ! su -c "otrs.Console.pl Maint::Database::Check" otrs 2> /tmp/console-maint-database-check.log; 
-do
-    egrep -o " Message: (.+)" /tmp/console-maint-database-check.log
+if [ $START_FRONTEND == '1' ]; then
+    while ! su -c "otrs.Console.pl Maint::Database::Check" otrs 2> /tmp/console-maint-database-check.log; 
+    do
+        egrep -o " Message: (.+)" /tmp/console-maint-database-check.log
 
-    # init configuration if empty
-    grep "database content is missing" /tmp/console-maint-database-check.log \
-    && [ $START_FRONTEND == '1' ] \
-    && su -c "/app-init.sh" otrs \
-    && otrs.SetPermissions.pl
-    
-    sleep 1;
-done
+        # init configuration if empty
+        grep "database content is missing" /tmp/console-maint-database-check.log \
+        && su -c "/app-init.sh" otrs \
+        && otrs.SetPermissions.pl
+
+        sleep 3;
+    done
+fi;
 
 if [ $START_SSHD != '0' ]; then
     if [ -z "$SSH_PASSWORD" ]; then
